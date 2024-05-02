d660b5f27b7f8320e28202234b4f5c3549597352
Jairo Zapata <jaimeconte687@gmail.com>
2024-05-01 20:41:44 -0500

    WIP

--- db.js
+++ db.js
@@ -1,25 +1,504 @@
 let carros = [
     {
-        nombre: "Toyota Corolla",
-        categoria: "Compacto",
-        imagen: "toyota_corolla.jpg",
-        codigoProducto: "TC001",
-        precio: 25000,
-        modelo: "2022",
-        transmicion: "Mecanico",
-        color: "Negro"
+        "nombre": "Sequoia",
+        "categoria": "Sitework & Site Utilities",
+        "imagen": "http://dummyimage.com/177x100.png/5fa2dd/ffffff",
+        "codigoProducto": "55301-271",
+        "precio": "$4.21B",
+        "modelo": 2011,
+        "transmicion": "5N1AA0NE3EN293517",
+        "color": "Teal"
     },
     {
-        nombre: "Ford Mustang",
-        categoria: "Deportivo",
-        imagen: "ford_mustang.jpg",
-        codigoProducto: "FM002",
-        precio: 40000,
-        modelo: "2023",
-        transmicion: "Automatico",
-        color: "Blanco"
+        "nombre": "Tacoma",
+        "categoria": "Plumbing & Medical Gas",
+        "imagen": "http://dummyimage.com/239x100.png/cc0000/ffffff",
+        "codigoProducto": "0145-1506",
+        "precio": "$437.47M",
+        "modelo": 1995,
+        "transmicion": "JM1CW2BL7D0130216",
+        "color": "Purple"
     },
-    
+    {
+        "nombre": "Millenia",
+        "categoria": "Epoxy Flooring",
+        "imagen": "http://dummyimage.com/222x100.png/cc0000/ffffff",
+        "codigoProducto": "25373-004",
+        "precio": "$283.98M",
+        "modelo": 1996,
+        "transmicion": "3G5DB03E92S595534",
+        "color": "Green"
+    },
+    {
+        "nombre": "Century",
+        "categoria": "Fire Sprinkler System",
+        "imagen": "http://dummyimage.com/176x100.png/ff4444/ffffff",
+        "codigoProducto": "0268-6723",
+        "precio": "$63.57M",
+        "modelo": 2004,
+        "transmicion": "WA1YD64B73N735341",
+        "color": "Purple"
+    },
+    {
+        "nombre": "Tracker",
+        "categoria": "Soft Flooring and Base",
+        "imagen": "http://dummyimage.com/210x100.png/dddddd/000000",
+        "codigoProducto": "50332-0102",
+        "precio": "$556.34M",
+        "modelo": 1995,
+        "transmicion": "WAUVC58E04A099166",
+        "color": "Purple"
+    },
+    {
+        "nombre": "Cougar",
+        "categoria": "Asphalt Paving",
+        "imagen": "http://dummyimage.com/133x100.png/dddddd/000000",
+        "codigoProducto": "36987-1313",
+        "precio": "$19.25B",
+        "modelo": 1989,
+        "transmicion": "1G6DC67A880367642",
+        "color": "Violet"
+    },
+    {
+        "nombre": "Element",
+        "categoria": "Waterproofing & Caulking",
+        "imagen": "http://dummyimage.com/146x100.png/dddddd/000000",
+        "codigoProducto": "42291-605",
+        "precio": "$202.5M",
+        "modelo": 2008,
+        "transmicion": "1C4RDJAG1DC454856",
+        "color": "Mauv"
+    },
+    {
+        "nombre": "Firebird Formula",
+        "categoria": "Soft Flooring and Base",
+        "imagen": "http://dummyimage.com/174x100.png/cc0000/ffffff",
+        "codigoProducto": "12634-518",
+        "precio": "$836.36M",
+        "modelo": 1992,
+        "transmicion": "JN8AF5MR9CT299309",
+        "color": "Red"
+    },
+    {
+        "nombre": "F-350 Super Duty",
+        "categoria": "Casework",
+        "imagen": "http://dummyimage.com/151x100.png/cc0000/ffffff",
+        "codigoProducto": "52380-4892",
+        "precio": "$95.04M",
+        "modelo": 2006,
+        "transmicion": "1G4PT5SV4F4402290",
+        "color": "Crimson"
+    },
+    {
+        "nombre": "Bronco",
+        "categoria": "Electrical and Fire Alarm",
+        "imagen": "http://dummyimage.com/201x100.png/cc0000/ffffff",
+        "codigoProducto": "59294-000",
+        "precio": "$21.91B",
+        "modelo": 1988,
+        "transmicion": "2C3CDXBGXFH092540",
+        "color": "Teal"
+    },
+    {
+        "nombre": "Tracker",
+        "categoria": "Wall Protection",
+        "imagen": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+        "codigoProducto": "55319-162",
+        "precio": "$90.19M",
+        "modelo": 2001,
+        "transmicion": "2G61W5S87F9075380",
+        "color": "Purple"
+    },
+    {
+        "nombre": "RS 4",
+        "categoria": "Structural and Misc Steel (Fabrication)",
+        "imagen": "http://dummyimage.com/195x100.png/dddddd/000000",
+        "codigoProducto": "67046-136",
+        "precio": "$1.87B",
+        "modelo": 2008,
+        "transmicion": "19UUA766X8A993176",
+        "color": "Blue"
+    },
+    {
+        "nombre": "62",
+        "categoria": "HVAC",
+        "imagen": "http://dummyimage.com/178x100.png/5fa2dd/ffffff",
+        "codigoProducto": "36987-2053",
+        "precio": "$4.32B",
+        "modelo": 2006,
+        "transmicion": "JN8AZ2KR7BT497703",
+        "color": "Turquoise"
+    },
+    {
+        "nombre": "Bonneville",
+        "categoria": "Temp Fencing, Decorative Fencing and Gates",
+        "imagen": "http://dummyimage.com/173x100.png/cc0000/ffffff",
+        "codigoProducto": "61958-1301",
+        "precio": "$12.03M",
+        "modelo": 1987,
+        "transmicion": "JTEBU5JR1F5672681",
+        "color": "Red"
+    },
+    {
+        "nombre": "SJ",
+        "categoria": "Painting & Vinyl Wall Covering",
+        "imagen": "http://dummyimage.com/216x100.png/5fa2dd/ffffff",
+        "codigoProducto": "54868-6118",
+        "precio": "$487.29M",
+        "modelo": 1992,
+        "transmicion": "1G6YV36A285765007",
+        "color": "Puce"
+    },
+    {
+        "nombre": "Toronado",
+        "categoria": "Rebar & Wire Mesh Install",
+        "imagen": "http://dummyimage.com/221x100.png/5fa2dd/ffffff",
+        "codigoProducto": "36987-3014",
+        "precio": "$7.13B",
+        "modelo": 1992,
+        "transmicion": "WBABD33435P944707",
+        "color": "Violet"
+    },
+    {
+        "nombre": "TundraMax",
+        "categoria": "Exterior Signage",
+        "imagen": "http://dummyimage.com/213x100.png/5fa2dd/ffffff",
+        "codigoProducto": "60429-554",
+        "precio": "$808.23M",
+        "modelo": 2010,
+        "transmicion": "5UXKR2C54F0973696",
+        "color": "Fuscia"
+    },
+    {
+        "nombre": "Tahoe",
+        "categoria": "Roofing (Asphalt)",
+        "imagen": "http://dummyimage.com/138x100.png/dddddd/000000",
+        "codigoProducto": "51367-011",
+        "precio": "$2.7B",
+        "modelo": 2010,
+        "transmicion": "2T1BPRHE3FC117370",
+        "color": "Red"
+    },
+    {
+        "nombre": "DeVille",
+        "categoria": "Landscaping & Irrigation",
+        "imagen": "http://dummyimage.com/165x100.png/dddddd/000000",
+        "codigoProducto": "55316-065",
+        "precio": "$370.12M",
+        "modelo": 1992,
+        "transmicion": "WBASP4C5XBC862000",
+        "color": "Goldenrod"
+    },
+    {
+        "nombre": "Continental",
+        "categoria": "Framing (Steel)",
+        "imagen": "http://dummyimage.com/118x100.png/5fa2dd/ffffff",
+        "codigoProducto": "49349-379",
+        "precio": "n/a",
+        "modelo": 1996,
+        "transmicion": "JTDKDTB30C1189799",
+        "color": "Yellow"
+    },
+    {
+        "nombre": "Brat",
+        "categoria": "Hard Tile & Stone",
+        "imagen": "http://dummyimage.com/112x100.png/5fa2dd/ffffff",
+        "codigoProducto": "10812-340",
+        "precio": "$2.77B",
+        "modelo": 1986,
+        "transmicion": "1G6YV36A775565691",
+        "color": "Red"
+    },
+    {
+        "nombre": "Dakota",
+        "categoria": "Asphalt Paving",
+        "imagen": "http://dummyimage.com/182x100.png/5fa2dd/ffffff",
+        "codigoProducto": "48951-1188",
+        "precio": "$81.49M",
+        "modelo": 2001,
+        "transmicion": "WDDUG7GB7FA615036",
+        "color": "Blue"
+    },
+    {
+        "nombre": "Avenger",
+        "categoria": "Sitework & Site Utilities",
+        "imagen": "http://dummyimage.com/167x100.png/5fa2dd/ffffff",
+        "codigoProducto": "37000-355",
+        "precio": "$145.49M",
+        "modelo": 1997,
+        "transmicion": "JN1AZ4EH3FM354955",
+        "color": "Teal"
+    },
+    {
+        "nombre": "LR3",
+        "categoria": "Retaining Wall and Brick Pavers",
+        "imagen": "http://dummyimage.com/156x100.png/ff4444/ffffff",
+        "codigoProducto": "0591-3703",
+        "precio": "$261.63M",
+        "modelo": 2006,
+        "transmicion": "WAUJC68E82A687303",
+        "color": "Fuscia"
+    },
+    {
+        "nombre": "VehiCROSS",
+        "categoria": "Rebar & Wire Mesh Install",
+        "imagen": "http://dummyimage.com/166x100.png/ff4444/ffffff",
+        "codigoProducto": "30142-685",
+        "precio": "$12.63M",
+        "modelo": 2001,
+        "transmicion": "2D4RN7DG9BR940480",
+        "color": "Pink"
+    },
+    {
+        "nombre": "Probe",
+        "categoria": "Structural & Misc Steel Erection",
+        "imagen": "http://dummyimage.com/183x100.png/dddddd/000000",
+        "codigoProducto": "65841-066",
+        "precio": "n/a",
+        "modelo": 1997,
+        "transmicion": "WUADNAFG4BN721525",
+        "color": "Indigo"
+    },
+    {
+        "nombre": "57",
+        "categoria": "Termite Control",
+        "imagen": "http://dummyimage.com/220x100.png/5fa2dd/ffffff",
+        "codigoProducto": "60913-017",
+        "precio": "$17.07M",
+        "modelo": 2007,
+        "transmicion": "WBAGL63453D010833",
+        "color": "Indigo"
+    },
+    {
+        "nombre": "Grand Vitara",
+        "categoria": "Structural and Misc Steel (Fabrication)",
+        "imagen": "http://dummyimage.com/145x100.png/5fa2dd/ffffff",
+        "codigoProducto": "76237-116",
+        "precio": "$568.38M",
+        "modelo": 2008,
+        "transmicion": "19UUA56793A804796",
+        "color": "Khaki"
+    },
+    {
+        "nombre": "S60",
+        "categoria": "Overhead Doors",
+        "imagen": "http://dummyimage.com/121x100.png/dddddd/000000",
+        "codigoProducto": "53603-1007",
+        "precio": "$152.21M",
+        "modelo": 2008,
+        "transmicion": "W04GS5EC4B1411367",
+        "color": "Goldenrod"
+    },
+    {
+        "nombre": "LeMans",
+        "categoria": "Fire Sprinkler System",
+        "imagen": "http://dummyimage.com/126x100.png/5fa2dd/ffffff",
+        "codigoProducto": "36800-066",
+        "precio": "$7.93B",
+        "modelo": 1965,
+        "transmicion": "WAUXL58E15A211452",
+        "color": "Indigo"
+    },
+    {
+        "nombre": "Torrent",
+        "categoria": "Glass & Glazing",
+        "imagen": "http://dummyimage.com/109x100.png/5fa2dd/ffffff",
+        "codigoProducto": "57955-5165",
+        "precio": "$113.53M",
+        "modelo": 2006,
+        "transmicion": "3VW467AT8CM246651",
+        "color": "Mauv"
+    },
+    {
+        "nombre": "400SE",
+        "categoria": "Elevator",
+        "imagen": "http://dummyimage.com/244x100.png/5fa2dd/ffffff",
+        "codigoProducto": "49873-403",
+        "precio": "$57.56B",
+        "modelo": 1992,
+        "transmicion": "2G4WN58C291808452",
+        "color": "Mauv"
+    },
+    {
+        "nombre": "Monte Carlo",
+        "categoria": "Plumbing & Medical Gas",
+        "imagen": "http://dummyimage.com/108x100.png/dddddd/000000",
+        "codigoProducto": "33992-0527",
+        "precio": "$8.36B",
+        "modelo": 2007,
+        "transmicion": "1G6DS5E34C0271260",
+        "color": "Orange"
+    },
+    {
+        "nombre": "S60",
+        "categoria": "Elevator",
+        "imagen": "http://dummyimage.com/167x100.png/ff4444/ffffff",
+        "codigoProducto": "62011-0045",
+        "precio": "$395.82M",
+        "modelo": 2010,
+        "transmicion": "WBAEJ13413A734729",
+        "color": "Green"
+    },
+    {
+        "nombre": "Savana",
+        "categoria": "Plumbing & Medical Gas",
+        "imagen": "http://dummyimage.com/200x100.png/5fa2dd/ffffff",
+        "codigoProducto": "75904-3246",
+        "precio": "n/a",
+        "modelo": 2006,
+        "transmicion": "YV1672MKXB2642417",
+        "color": "Indigo"
+    },
+    {
+        "nombre": "Q",
+        "categoria": "Marlite Panels (FED)",
+        "imagen": "http://dummyimage.com/128x100.png/cc0000/ffffff",
+        "codigoProducto": "49781-054",
+        "precio": "n/a",
+        "modelo": 1995,
+        "transmicion": "SCFFDCBD3CG749498",
+        "color": "Orange"
+    },
+    {
+        "nombre": "Rocky",
+        "categoria": "Masonry",
+        "imagen": "http://dummyimage.com/156x100.png/cc0000/ffffff",
+        "codigoProducto": "55154-5627",
+        "precio": "$2.17B",
+        "modelo": 1992,
+        "transmicion": "W04GT5GCXB1771415",
+        "color": "Purple"
+    },
+    {
+        "nombre": "XG350",
+        "categoria": "Plumbing & Medical Gas",
+        "imagen": "http://dummyimage.com/109x100.png/dddddd/000000",
+        "codigoProducto": "0268-6702",
+        "precio": "$415.87M",
+        "modelo": 2002,
+        "transmicion": "WVWAP7AN1EE285305",
+        "color": "Green"
+    },
+    {
+        "nombre": "D350",
+        "categoria": "Structural & Misc Steel Erection",
+        "imagen": "http://dummyimage.com/194x100.png/ff4444/ffffff",
+        "codigoProducto": "64679-422",
+        "precio": "$386.9M",
+        "modelo": 1993,
+        "transmicion": "1G4GA5ER9CF155255",
+        "color": "Pink"
+    },
+    {
+        "nombre": "Protege",
+        "categoria": "RF Shielding",
+        "imagen": "http://dummyimage.com/210x100.png/ff4444/ffffff",
+        "codigoProducto": "24275-0385",
+        "precio": "$113.08B",
+        "modelo": 1998,
+        "transmicion": "1N6AA0EC0EN818131",
+        "color": "Purple"
+    },
+    {
+        "nombre": "Optima",
+        "categoria": "Electrical and Fire Alarm",
+        "imagen": "http://dummyimage.com/132x100.png/dddddd/000000",
+        "codigoProducto": "11673-271",
+        "precio": "n/a",
+        "modelo": 2011,
+        "transmicion": "JTHBB1BA7B2302518",
+        "color": "Pink"
+    },
+    {
+        "nombre": "MGB",
+        "categoria": "Roofing (Metal)",
+        "imagen": "http://dummyimage.com/188x100.png/ff4444/ffffff",
+        "codigoProducto": "41250-605",
+        "precio": "n/a",
+        "modelo": 1977,
+        "transmicion": "WA1CMAFE8DD482509",
+        "color": "Indigo"
+    },
+    {
+        "nombre": "Ram 1500 Club",
+        "categoria": "Drilled Shafts",
+        "imagen": "http://dummyimage.com/171x100.png/cc0000/ffffff",
+        "codigoProducto": "49999-343",
+        "precio": "n/a",
+        "modelo": 1995,
+        "transmicion": "SCBZU25E31C205858",
+        "color": "Red"
+    },
+    {
+        "nombre": "Camaro",
+        "categoria": "Fire Sprinkler System",
+        "imagen": "http://dummyimage.com/153x100.png/cc0000/ffffff",
+        "codigoProducto": "37808-291",
+        "precio": "$111.85M",
+        "modelo": 2001,
+        "transmicion": "SCBDR33W99C459258",
+        "color": "Turquoise"
+    },
+    {
+        "nombre": "960",
+        "categoria": "Plumbing & Medical Gas",
+        "imagen": "http://dummyimage.com/157x100.png/5fa2dd/ffffff",
+        "codigoProducto": "13537-196",
+        "precio": "$2.15B",
+        "modelo": 1996,
+        "transmicion": "WBAGH03461D367935",
+        "color": "Blue"
+    },
+    {
+        "nombre": "H3",
+        "categoria": "Structural and Misc Steel (Fabrication)",
+        "imagen": "http://dummyimage.com/219x100.png/cc0000/ffffff",
+        "codigoProducto": "60681-1806",
+        "precio": "$39.48B",
+        "modelo": 2007,
+        "transmicion": "1N6AA0EJ9FN851998",
+        "color": "Fuscia"
+    },
+    {
+        "nombre": "Concorde",
+        "categoria": "Waterproofing & Caulking",
+        "imagen": "http://dummyimage.com/119x100.png/dddddd/000000",
+        "codigoProducto": "54569-4391",
+        "precio": "$33.71M",
+        "modelo": 2004,
+        "transmicion": "WBAYF4C57FD328207",
+        "color": "Blue"
+    },
+    {
+        "nombre": "Bonneville",
+        "categoria": "Prefabricated Aluminum Metal Canopies",
+        "imagen": "http://dummyimage.com/235x100.png/dddddd/000000",
+        "codigoProducto": "51079-078",
+        "precio": "n/a",
+        "modelo": 1997,
+        "transmicion": "JH4CU2F69CC558035",
+        "color": "Mauv"
+    },
+    {
+        "nombre": "Bonneville",
+        "categoria": "Rebar & Wire Mesh Install",
+        "imagen": "http://dummyimage.com/173x100.png/ff4444/ffffff",
+        "codigoProducto": "55910-799",
+        "precio": "$3.72B",
+        "modelo": 1998,
+        "transmicion": "SAJWA4DC3BM200239",
+        "color": "Crimson"
+    },
+    {
+        "nombre": "Sable",
+        "categoria": "Granite Surfaces",
+        "imagen": "http://dummyimage.com/197x100.png/cc0000/ffffff",
+        "codigoProducto": "54868-5440",
+        "precio": "$598.91M",
+        "modelo": 2009,
+        "transmicion": "WBADN53483G222289",
+        "color": "Aquamarine"
+    }
 ];
 
 const handleSubmit = (event) => {
